1b3bb709c18df7802515acd623fbaf817afbe477
ysaiascampos <ysaiascampos@gmail.com>
2021-04-12 21:19:11 -0400

    useReducer

--- App.js
+++ App.js
@@ -1,27 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState, useEffect } from 'react';
+import React, { useReducer } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+const initialState = {
+  cont: 0
+}
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'incrementar':{
+      return {
+        cont: state.cont + 1
+      }
+    }
+    case 'decrementar':{
+      return {
+        cont: state.cont - 1
+      }
+    }
+    default: {
+      return state
+    }
+  }
+}
+
 export default function App() {
-  const [loading, setLoading] = useState(true);
-  const [users, setUsers] = useState([]);
-
-  const fetchUsers = async () => {
-    const response = await fetch('http://jsonplaceholder.typicode.com/users');
-    const json = await response.json();
-    setUsers(json);
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    fetchUsers();
-  },[]);
+  const [state, dispatch] = useReducer(reducer, initialState);
   return (
     <View style={styles.container}>
-      <Text onPress={() => setCont(cont + 1)} style={styles.text}>
-        {loading?'Cargando...'
-        : users[0].name
-        }
+      <Text onPress={() => dispatch({type: 'incrementar'})} style={styles.text}>
+        +
+      </Text>
+      <Text style={styles.text}>
+        {state.cont}
+      </Text>
+      <Text onPress={() => dispatch({type: 'decrementar'})} style={styles.text}>
+        -
       </Text>
       <StatusBar style="auto" />
     </View>
@@ -36,6 +50,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   text: {
-    fontSize: 35,
+    fontSize: 60,
   },
 });
