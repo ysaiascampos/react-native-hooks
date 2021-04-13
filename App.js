11b65f2397616e9fbf3df06a7a1114a3ae145f1b
ysaiascampos <ysaiascampos@gmail.com>
2021-04-12 21:36:00 -0400

    useCallback

--- App.js
+++ App.js
@@ -1,15 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   const [cont, setCont] = useState(0)
-  const incrementar = () => {
+
+  const incrementar = useCallback(() => {
     setCont(cont + 1)
-  }
-  const decrementar = () => {
+  }, [cont])
+  const decrementar = useCallback(() => {
     setCont(cont - 1)
-  }
+  }, [cont])
   return (
     <View style={styles.container}>
       <Text onPress={() => incrementar()} style={styles.text}>
