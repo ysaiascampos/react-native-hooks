2f4c4d598ef86818e40f1e0d3f5dc6c2f85a8df3
ysaiascampos <ysaiascampos@gmail.com>
2021-04-12 21:33:03 -0400

    useState

--- App.js
+++ App.js
@@ -1,52 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useReducer, useMemo } from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-const initialState = {
-  cont: 0
-}
-
-const reducer = (state, action) => {
-  switch (action.type) {
-    case 'incrementar':{
-      return {
-        cont: state.cont + 1
-      }
-    }
-    case 'decrementar':{
-      return {
-        cont: state.cont - 1
-      }
-    }
-    default: {
-      return state
-    }
-  }
-}
-
-const users = [{ name: 'el', age: 2 },{ name: 'ella', age: 3 }]
-
 export default function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
-
-  const totalAge = useMemo(() => {
-    let age = 0;
-    console.log('calculando..');
-    users.forEach(x => {
-      age= age + x.age
-    });
-    return age;
-  }, [users])
-  console.log('edad total', totalAge);
+  const [cont, setCont] = useState(0)
+  const incrementar = () => {
+    setCont(cont + 1)
+  }
+  const decrementar = () => {
+    setCont(cont - 1)
+  }
   return (
     <View style={styles.container}>
-      <Text onPress={() => dispatch({type: 'incrementar'})} style={styles.text}>
+      <Text onPress={() => incrementar()} style={styles.text}>
         +
       </Text>
       <Text style={styles.text}>
-        {state.cont}
+        {cont}
       </Text>
-      <Text onPress={() => dispatch({type: 'decrementar'})} style={styles.text}>
+      <Text onPress={() => decrementar()} style={styles.text}>
         -
       </Text>
       <StatusBar style="auto" />
