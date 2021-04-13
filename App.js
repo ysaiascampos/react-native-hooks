f875672cdd3a00c8a419b0948712f249e5876416
ysaiascampos <ysaiascampos@gmail.com>
2021-04-12 21:06:31 -0400

    useEffect

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
+  const [users, setUsers] = useState([]);
+
+  const fetchUsers = async () => {
+    const response = await fetch('http://jsonplaceholder.typicode.com/users');
+    const json = await response.json();
+    setUsers(json);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  },[]);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text onPress={() => setCont(cont + 1)} style={styles.text}>
+        {loading?'Cargando...'
+        : users[0].name
+        }
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +34,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    fontSize: 35,
   },
 });
